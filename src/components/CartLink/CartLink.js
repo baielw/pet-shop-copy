5c2afb59f563b083d74c65aff8226e141a763be5
baielw <baielrozbekov@gmail.com>
2022-06-29 10:38:49 +0600

    add cartlogo

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import cartImage from "../../assets/cart.webp";
+import cartImage from "../../assets/cart.png";
 import "./CartLink.css";
 
 export default function CartLink() {
-  const number = useSelector((store) => {
+  const number = useSelector((store) => { 
     return Object.values(store.cart.items).reduce(
       (sum, number) => sum + number,
       0
